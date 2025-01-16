a96f683f009c9bc60cfcee811f02f143d7e626cd
Mitchell Beare <Mitchell.Beare@sedgman.com>
2025-01-17 09:59:40 +1000

    Adjusted statusline to show more information

--- lua/beare/plugins/rosepine.lua
+++ lua/beare/plugins/rosepine.lua
@@ -1,18 +1,27 @@
 return {
-	'rose-pine/neovim',
-	name = 'rose-pine',
-	config = function()
-		require("rose-pine").setup({
-			styles = {
-				transparency = true,
-			},
-			highlight_groups = {
-				StatusLine = { fg = "love", bg = "love", blend = 10},
-				StatusLineNC = {fg = "subtle", bg = "surface"},
-			},
-		})
-		vim.opt.laststatus = 3
-		vim.opt.statusline = " %f %m %= %l:%c ♥ "
-		vim.cmd('colorscheme rose-pine-moon')
-	end
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+        require("rose-pine").setup({
+            extend_background_behind_borders = true,
+            enable = {
+                terminal = true,
+            },
+            styles = {
+                transparency = true,
+            },
+            highlight_groups = {
+                StatusLine = { fg = "Love", bg = "Rose", blend = 5 },
+                StatusLineNC = { fg = "subtle", bg = "surface" },
+            },
+        })
+        vim.opt.laststatus = 3
+        -- %f file in buffer with path
+        -- %m is the file modified? [-] [+]
+        -- %= justify to the right from here
+        -- %l line number
+        -- %c column number
+        vim.opt.statusline = " %{fugitive#statusline()} > %.50f %m - FileType: %y %= %{mode()} [%l↓ %c→] 🐻 "
+        vim.cmd('colorscheme rose-pine-moon')
+    end
 }
